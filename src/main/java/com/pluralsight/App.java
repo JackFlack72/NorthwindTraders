62ae8647a795f5c4cc074b89e1d88db1d5d959f4
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-12-02 10:19:13 -0600

    add prepared statement and close the database

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,3 +1,4 @@
+
 package com.pluralsight;
 
 import java.sql.*;
@@ -5,30 +6,48 @@ import java.sql.*;
 public class App {
 
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/northwind";
+        String url = "jdbc:mysql://localhost:3306/sakila";
         String username = args[0];
         String password = args[1];
 
         //1. open a connection to the database
         Connection connection = DriverManager.getConnection(url, username, password);
 
-        // create statement
-        // the statement is tied to the open connection
-        Statement statement = connection.createStatement();
 
         // define your query
-        String query = "SELECT ProductName FROM products";
+        String query = """
+                        SELECT title, description, release_year, length
+                        FROM film
+                        WHERE title LIKE ?
+                        """;
+
+        // create statement
+        // the statement is tied to the open connection
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        String searchTerm = "%AIR%";
+        //set parameters
+        statement.setString(1,  searchTerm);
 
         // 2. Execute your query
-        ResultSet results = statement.executeQuery(query);
+        ResultSet results = statement.executeQuery();
 
         // process the results
         while (results.next()) {
-            String products = results.getString("ProductName");
-            System.out.println(products);
+            String title = results.getString("title");
+            String description = results.getString("description");
+            int releaseYear = results.getInt("release_year");
+            int length = results.getInt("length");
+
+            System.out.println(title);
+            System.out.println(description);
+            System.out.println(releaseYear);
+            System.out.println(length);
+            System.out.println("-----------------------");
         }
 
-        // 3. Close the connection
+        results.close();
+        statement.close();
         connection.close();
 
     }
