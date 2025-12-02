98b5dec40703321886a8bfcd4c859999463bf10c
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-12-02 15:16:18 -0600

    add methods for displaying products and customers

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -5,50 +5,84 @@ import java.sql.*;
 
 public class App {
 
-    public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/sakila";
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/northwind";
         String username = args[0];
         String password = args[1];
 
-        //1. open a connection to the database
-        Connection connection = DriverManager.getConnection(url, username, password);
-
-
-        // define your query
-        String query = """
-                        SELECT title, description, release_year, length
-                        FROM film
-                        WHERE title LIKE ?
-                        """;
-
-        // create statement
-        // the statement is tied to the open connection
-        PreparedStatement statement = connection.prepareStatement(query);
-
-        String searchTerm = "%AIR%";
-        //set parameters
-        statement.setString(1,  searchTerm);
-
-        // 2. Execute your query
-        ResultSet results = statement.executeQuery();
-
-        // process the results
-        while (results.next()) {
-            String title = results.getString("title");
-            String description = results.getString("description");
-            int releaseYear = results.getInt("release_year");
-            int length = results.getInt("length");
-
-            System.out.println(title);
-            System.out.println(description);
-            System.out.println(releaseYear);
-            System.out.println(length);
-            System.out.println("-----------------------");
-        }
-
-        results.close();
-        statement.close();
-        connection.close();
+        displayProducts(url, username, password);
+        displayCustomers(url, username, password);
 
     }
+
+    private static void displayProducts(String url, String username, String password) {
+        String query = """
+                SELECT productid, productname, unitprice, unitsinstock
+                FROM products
+                WHERE productname LIKE ?
+                """;
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            String searchTerm = "a%";
+            statement.setString(1, searchTerm);
+
+            try (ResultSet results = statement.executeQuery()) {
+
+                while (results.next()) {
+                    String productId = results.getString("productid");
+                    String name = results.getString("productname");
+                    double price = results.getDouble("unitprice");
+                    int stock = results.getInt("unitsinstock");
+
+                    System.out.println("Product Id: " + productId);
+                    System.out.println("Name: " + name);
+                    System.out.println("Price: $" + price);
+                    System.out.println("Stock: " + stock);
+                    System.out.println("-----------------------");
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("There was an error retrieving the data. Please try again.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void displayCustomers(String url, String username, String password) {
+        String query = """
+                SELECT CustomerID, CompanyName, ContactName, ContactTitle
+                FROM customers
+                WHERE ContactName LIKE ?
+                """;
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            String searchTerm = "a%";
+            statement.setString(1, searchTerm);
+
+            try (ResultSet results = statement.executeQuery()) {
+
+                while (results.next()) {
+                    String customerId = results.getString("CustomerID");
+                    String companyName = results.getString("CompanyName");
+                    String contactName = results.getString("ContactName");
+                    String contactTitle = results.getString("ContactTitle");
+
+                    System.out.println("Customer Id: " + customerId);
+                    System.out.println("Company Name: " + companyName);
+                    System.out.println("Customer Name: " + contactName);
+                    System.out.println("Customer Title: " + contactTitle);
+                    System.out.println("-----------------------");
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("There was an error retrieving the data. Please try again.");
+            e.printStackTrace();
+        }
+    }
+
 }
